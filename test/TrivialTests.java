65cd27717954ce8e92cf366ef9e405314b191e23
Javier Moragues Fernández <javier.moraguesfernandez@iesfm.onmicrosoft.com>
2022-02-18 09:16:54 +0100

    Refactorizacion metodo NuevaPosicionJugador y creacion de Test

--- test/TrivialTests.java
+++ test/TrivialTests.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import smellytrivial.Game;
@@ -5,11 +6,26 @@ import smellytrivial.Game;
 public class TrivialTests {
     @Test
     public void true_is_true(){
-        Assertions.assertTrue(false);
+        Assertions.assertTrue(true);
     }
 
     @Test
     public void crear_Game(){
         Game trivial = new Game();
+    }
+
+    @Test
+    public void si_al_principio_saco_un_1_voy_a_la_casilla_1 (){
+        //Preparación
+        Game juego=new Game();
+        juego.agregar("Maria");
+        juego.agregar("Juan");
+
+        juego.tirarDado(1);
+        String mensajePosicion="La nueva posición de Maria es 1";
+        //Ejecucion
+        String result=juego.nuevaPosicionJugador();
+        //Comprobación
+        Assert.assertEquals(mensajePosicion,result);
     }
 }
